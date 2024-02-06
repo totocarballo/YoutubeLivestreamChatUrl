af0e242048378765eb8a710bd3ab0ebb8eb53ccf
Toto <Toto@DESKTOP-A6SR02L>
2024-02-05 22:44:30 -0300

    Agregar archivos de proyecto.

---
+++ Livestream.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace YoutubeLivestreamChatUrl
+{
+    internal class Livestream
+    {
+        public static string GetChatUrl(string channelUrl)
+        {
+            Console.Clear();
+
+            string username = channelUrl;
+            string pattern = username.Contains("channel") ? @"(?:\/channel\/)([^\/]+)(?:\/.*)?$" : @"(?:youtube\.com\/)([^\/]+)";
+
+            var match = Regex.Match(username, pattern);
+
+            if (!match.Success)
+            {
+                return "Channel URL isn't in the correct format";
+            }
+
+            try
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    string htmlContent = webClient.DownloadString($"https://www.youtube.com/{match.Groups[1].Value}/live");
+                    string url = ParseChatUrl(htmlContent);
+                    if (string.IsNullOrEmpty(url))
+                    {
+                        return "The channel isn't livestreaming right now.";
+                    }
+                    return url;
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex}";
+            }
+        }
+
+        private static string ParseChatUrl(string htmlContent)
+        {
+            int indexUrl = htmlContent.IndexOf("<link rel=\"canonical\" href=\"https://www.youtube.com/watch?v=");
+            
+            if (indexUrl != -1)
+            {
+                //Get the title of the stream:
+                
+                //int indexStartTitle = htmlContent.IndexOf("\"title\" content=\"") + 17;
+                //int indexEndTitle = htmlContent.IndexOf("\">", indexStartTitle);
+                //string title = htmlContent.Substring(indexStartTitle, indexEndTitle - indexStartTitle);
+
+                int indexStartUrl = indexUrl + 60;
+                int indexEndUrl = htmlContent.IndexOf("\"", indexStartUrl);
+                return $"https://www.youtube.com/live_chat?is_popout=1&v={htmlContent.Substring(indexStartUrl, indexEndUrl - indexStartUrl)}";
+            }
+            
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace YoutubeLivestreamChatUrl
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Get current public livestream chat Url from Youtube channel.");
+            Console.WriteLine("Compatible formats:");
+            Console.WriteLine("  https://www.youtube.com/@YouTube");
+            Console.WriteLine("  youtube.com/YouTube");
+            Console.WriteLine($"  https://www.youtube.com/channel/UCBR8-60-B28hp2BmDPdntcQ");
+            Console.WriteLine($"  youtube.com/channel/UCBR8-60-B28hp2BmDPdntcQ\n");
+
+            Console.WriteLine("Insert Youtube channel:");
+            var url = Console.ReadLine().ToLower();
+
+            if (url.Contains("youtube.com/"))
+            {
+                var livestream = Livestream.GetChatUrl(url);
+
+                Console.WriteLine($"This is the live stream URL:\n{livestream}\n\nCopy to the clipboard?: (y/n)");
+                var key = Console.ReadKey().Key.ToString().ToLower();
+
+                if (key.Equals("y"))
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = "clip.exe",
+                        RedirectStandardInput = true,
+                        UseShellExecute = false
+                    }).StandardInput.Write(livestream);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Check the url and the compatible formats before use it.");
+            }
+        }
+    }
+}
